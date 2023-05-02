a5890f3337978670cd4b75c9d17044f6946ce70f
JimmyDillon-CS <67019500+JimmyDillon-CS@users.noreply.github.com>
2023-05-02 09:54:46 -0400

    Create make_pokedex.py

---
+++ make_pokedex.py
@@ -0,0 +1,19 @@
+#paste the output (or pipe the output) of this program to the pokedex.html file.
+
+site = '''
+<DOCTYPE HTML>
+<html>
+  <head>
+    <title>
+      ?TITLE?
+    </title>
+    <style>
+      ?STYLE?
+    </style>
+  </head>
+  <body>
+    ?BODY?
+  </body>
+</html>'''
+
+print(site)
